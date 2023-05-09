a2eac01d7ecc1619f8364b8cfd673cd6a4b349f3
Grzegorz Cyuńczyk <63702299+cyunczykg@users.noreply.github.com>
2023-05-09 12:08:38 +0200

    Added text size and color picker
    Additional +/- buttons for box text size modification and color input.

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ const btnBlue = document.querySelector('.btn2');
 const btnClear = document.querySelector('.remove-color');
 const btnJump = document.querySelector('.btn3');
 const colorfullSquere = document.querySelector('.color');
+const btnplus = document.querySelector('#btnplus');
+const btnminus = document.querySelector('#btnminus');
+const colorfullSquereText = document.querySelector('.color');
+let currentFontSize = 20;
+let pickedcolor = document.getElementById('textcolorpicker');
 
 function redColor() {
 	colorfullSquere.classList.remove('blue');
@@ -26,7 +31,28 @@ function animateSquere() {
 	}, 1500);
 }
 
+function sizeUp() {
+	if (currentFontSize < 65) {
+		currentFontSize += 5;
+	}
+	colorfullSquere.style.fontSize = currentFontSize + 'px';
+}
+
+function sizeDown() {
+	if (currentFontSize > 5) {
+		currentFontSize -= 5;
+	}
+	colorfullSquere.style.fontSize = currentFontSize + 'px';
+}
+
+function setTextColor() {
+	colorfullSquere.style.color = pickedcolor.value;
+}
+
 btnRed.addEventListener('click', redColor);
 btnBlue.addEventListener('click', blueColor);
 btnClear.addEventListener('click', clearColor);
 btnJump.addEventListener('click', animateSquere);
+btnplus.addEventListener('click', sizeUp);
+btnminus.addEventListener('click', sizeDown);
+pickedcolor.addEventListener('input', setTextColor);
